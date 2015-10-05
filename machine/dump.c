b6ec18bd65deaa2117acca636af5e1edfe952a8a
Pauline DJAMEI <pdjamei@e2r5p5.42.fr>
2015-10-05 11:52:25 +0200

    affichage de la memoire au format hexadecimal

--- machine/dump.c
+++ machine/dump.c
@@ -16,10 +16,41 @@
 #include "arena.h"
 #include "libft.h"
 
+static void		ft_puthex(unsigned char c)
+{
+	if ((c / 16) < 10)
+		ft_putnbr(c / 16);
+	else
+		ft_putchar('A' + c / 16 - 10);
+	if (c % 16 < 10)
+		ft_putnbr(c % 16);
+	else
+		ft_putchar('A' + c % 16 - 10);
+}
 
 void			ft_dump(unsigned char *arena, t_champdata *champ, t_cycle *cycle)
 {
-	write(1, arena, MEM_SIZE);
+	int i;
+
+	i = 0;
+	while (i < MEM_SIZE)
+	{
+		if (i % 32 != 31)
+		{
+			ft_putnbr(0);
+			ft_putchar('x');
+			ft_puthex(arena[i]);
+			ft_putchar(' ');
+		}
+		else
+		{
+			ft_putnbr(0);
+			ft_putchar('x');
+			ft_puthex(arena[i]);
+			ft_putchar('\n');
+		}
+		i++;
+	}
 	(void)champ;
 	(void)cycle;
 }
